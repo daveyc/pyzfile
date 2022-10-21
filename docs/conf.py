0ffd383196bf50b8fb26a3542c15742bd172b2d1
David Crayford <dcrayford@rocketsoftware.com>
2022-10-21 15:25:43 +0800

    Fix doc build

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('pyzfile'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
